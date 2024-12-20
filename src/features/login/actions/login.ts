69697a6cdb8e5fd45f579ac89fbd61922a8aeeed
hasegawa-101 <hase1225hayato@gmail.com>
2024-12-21 02:51:32 +0900

    コメント追加

--- src/features/login/actions/login.ts
+++ src/features/login/actions/login.ts
@@ -4,12 +4,16 @@ import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
 import { loginSchema } from "@/schemas/schemas";
 
+// サーバー側でもバリデーションを行う
+// バリデーションが成功したらリダイレクトする
+// バリデーションが失敗したら submission.reply() を返す
 export async function login(prevState: unknown, formData: FormData) {
   const submission = parseWithZod(formData, {
     schema: loginSchema,
   });
 
   if (submission.status !== "success") {
+    // reply()の結果は useActionState の state に渡される
     return submission.reply();
   }
 
